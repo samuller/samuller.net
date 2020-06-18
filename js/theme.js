fa727b0b90a31b024dbaf047e3290aac28561ec0
Simon Muller <samullers@gmail.com>
2020-06-19 00:35:31 +0200

    Add new 'white' theme

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,13 @@
 ---
 ---
 
-function toggleTheme(){
+/**
+ * Iterate between possible themes.
+ */
+function switchTheme(){
+    let themes = ['light', 'dark', 'white'];
     let currTheme = document.documentElement.getAttribute('data-theme');
-    let newTheme = currTheme == 'light' ? 'dark' : 'light';
+    let currIdx = Math.max(themes.indexOf(currTheme), 0);
+    let newTheme = themes[(currIdx + 1) % themes.length];
     document.documentElement.setAttribute('data-theme', newTheme);
 }
